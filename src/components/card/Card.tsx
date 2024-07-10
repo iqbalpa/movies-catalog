82029f5f738d142954429bba824ee4fa371664a6
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-10 15:24:12 +0700

    refactor(home): create card movie component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import Image from 'next/image';
+import { Movie } from '@/constant/movie';
 
-interface CardProps {
-  title: string;
-  content: string;
+interface ICard {
+  movie: Movie;
 }
 
-const Card: React.FC<CardProps> = ({ title, content }) => {
+const baseUrl = 'https://image.tmdb.org/t/p/original';
+
+const Card: React.FC<ICard> = ({ movie }) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{content}</p>
+    <div className="relative w-[300px] overflow-hidden rounded-xl bg-slate-300">
+      <div className="absolute inset-0 z-10 bg-black bg-opacity-20"></div>
+      <div className="absolute left-0 top-0 z-20 w-full p-4 text-center text-white">
+        <p className="font-bold">
+          {movie.title} ({movie?.release_date.split('-')[0]})
+        </p>
+      </div>
+      <Image
+        src={`${baseUrl}${movie.poster_path}`}
+        alt={movie.title}
+        width={300}
+        height={50}
+        className="object-cover"
+      />
     </div>
   );
 };
