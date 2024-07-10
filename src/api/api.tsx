591b196ddfe17630e7f48d491a7ddc1ca60256c9
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-10 18:47:40 +0700

    feat(home): add pagination

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -5,9 +5,9 @@ const BASE_URL = 'https://api.themoviedb.org/3/discover/movie';
 const API_KEY =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzM0Njg0NjI2OTg1ODI3OTE1NzMyNWY5OTAxZmU4ZCIsIm5iZiI6MTcyMDU4OTg3MC4wMzYzNDIsInN1YiI6IjY2OGUxY2JlMzA0OTRhNmE2OTMyYzE3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jkIXGMw1WWmtEO9XDryk4R5gP35WQIZHQ8uGIH-QgnM';
 
-const getAllMovies = async (): Promise<Movie[]> => {
+const getAllMovies = async (currentPage: number): Promise<Movie[]> => {
   const res = await axios.get(
-    `${BASE_URL}?include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
+    `${BASE_URL}?include_adult=true&include_video=false&language=en-US&page=${currentPage}&sort_by=popularity.desc`,
     {
       headers: {
         Accept: 'application/json',
--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -6,15 +6,25 @@ import { Movie } from '@/constant/movie';
 import Header from '@/components/header/header';
 import Backdrop from '@/components/backdrop/backdrop';
 import ListMovies from '@/components/listMovies/listMovies';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
 
 const HomeModule: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [backdrop, setBackdrop] = useState<string>('');
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const res: Movie[] = await api.getAllMovies();
+        const res: Movie[] = await api.getAllMovies(currentPage);
         console.log(res);
         setMovies(res);
         setBackdrop(
@@ -26,13 +36,61 @@ const HomeModule: React.FC = () => {
       }
     };
     fetchMovies();
-  }, []);
+  }, [currentPage]);
+
+  const maxPage = Math.floor(948785 / 20);
+  const handleClickPrev = () => {
+    setCurrentPage((prevPage) => {
+      if (prevPage === 1) {
+        return 1;
+      }
+      return prevPage - 1;
+    });
+  };
+  const handleClickNext = () => {
+    setCurrentPage((prevPage) => {
+      if (prevPage === maxPage) {
+        return maxPage;
+      }
+      return prevPage + 1;
+    });
+  };
 
   return (
     <div className="flex min-h-screen w-full flex-col pb-20">
       <Header />
       <Backdrop movie={movies[2]} backdrop={backdrop} />
       <ListMovies movies={movies} />
+
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              onClick={handleClickPrev}
+              className="hover:cursor-pointer"
+            />
+          </PaginationItem>
+          {currentPage > 1 && (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          )}
+          <PaginationItem>
+            <PaginationLink>{currentPage}</PaginationLink>
+          </PaginationItem>
+          {currentPage < maxPage && (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          )}
+          <PaginationItem>
+            <PaginationNext
+              onClick={handleClickNext}
+              className="hover:cursor-pointer"
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 };
