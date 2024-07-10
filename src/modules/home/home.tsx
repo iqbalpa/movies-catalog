ebaf89f4bdd7c0eab6f1a17f413111743228a561
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-10 13:02:33 +0700

    feat: create header

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const HomeModule: React.FC = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="flex min-h-screen w-full flex-col bg-sky-300">
+      {/* Header */}
+      <div className="fixed w-full bg-slate-800 p-4">
+        <h1 className="text-2xl font-bold text-white">Movies Catalog</h1>
+      </div>
     </div>
   );
 };
