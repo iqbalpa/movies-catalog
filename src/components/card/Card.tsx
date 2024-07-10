3707a18ca798d4cf471d5352e4f76cfcdd390d61
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-10 19:06:28 +0700

    feat(home): add skeleton for card movie

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { Movie } from '@/constant/movie';
+import { Skeleton } from '../ui/skeleton';
 
 interface ICard {
   movie: Movie;
@@ -9,20 +10,25 @@ interface ICard {
 const baseUrl = 'https://image.tmdb.org/t/p/original';
 
 const Card: React.FC<ICard> = ({ movie }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <div className="relative w-[300px] overflow-hidden rounded-xl bg-slate-300">
+    <div className="relative h-[450px] w-[300px] overflow-hidden rounded-xl">
       <div className="absolute inset-0 z-10 bg-black bg-opacity-20"></div>
       <div className="absolute left-0 top-0 z-20 w-full p-4 text-center text-white">
         <p className="font-bold">
           {movie.title} ({movie?.release_date.split('-')[0]})
         </p>
       </div>
+      {isLoading && <Skeleton className="absolute inset-0 h-full w-full" />}
       <Image
         src={`${baseUrl}${movie.poster_path}`}
         alt={movie.title}
         width={300}
-        height={50}
-        className="object-cover"
+        height={450}
+        className={`object-cover transition-opacity duration-500 ${isLoading ? 'opacity-0' : 'opacity-100'}`}
+        onLoadingComplete={() => setIsLoading(false)}
+        onLoad={() => setIsLoading(false)}
       />
     </div>
   );
