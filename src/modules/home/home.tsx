c50134b49c393d1175a146ce575a137a88102aeb
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-10 14:50:14 +0700

    feat: create list of movies

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -5,6 +5,8 @@ import api from '@/api/api';
 import { Movie } from '@/constant/movie';
 import Image from 'next/image';
 
+const baseUrl = 'https://image.tmdb.org/t/p/original';
+
 const HomeModule: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [movie, setMovie] = useState<Movie>();
@@ -29,7 +31,7 @@ const HomeModule: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex min-h-screen w-full flex-col bg-sky-300">
+    <div className="flex min-h-screen w-full flex-col pb-20">
       {/* Header */}
       <div className="fixed z-30 h-16 w-full bg-slate-800 p-4">
         <h1 className="text-2xl font-bold text-white">Movies Catalog</h1>
@@ -52,20 +54,25 @@ const HomeModule: React.FC = () => {
           className="z-10"
         />
       </div>
-      {/* <div className="relative mt-16 h-[28rem] w-full">
-        <p className="text-3xl font-bold text-white">{movie?.title}</p>
-        <Image
-          src={backdrop}
-          alt="backdrop-image"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div> */}
-      <div className="mt-16 flex flex-col gap-2 p-2">
+
+      {/* List of Movies */}
+      <div className="grid grid-cols-4 gap-5 px-28 py-10">
         {movies.map((movie, index) => (
-          <p key={movie.id}>
-            {movie.title} ({movie.release_date})
-          </p>
+          <div className="relative w-[300px] overflow-hidden rounded-xl bg-slate-300">
+            <div className="absolute inset-0 z-10 bg-black bg-opacity-20"></div>
+            <div className="absolute left-0 top-0 z-20 w-full p-4 text-center text-white">
+              <p className="font-bold">
+                {movie.title} ({movie?.release_date.split('-')[0]})
+              </p>
+            </div>
+            <Image
+              src={`${baseUrl}${movie.poster_path}`}
+              alt={movie.title}
+              width={300}
+              height={50}
+              className="object-cover"
+            />
+          </div>
         ))}
       </div>
     </div>
