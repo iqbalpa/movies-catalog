45ddafb413dbd1b01d0521160375b403bdbae5ac
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-11 06:17:14 +0700

    chore: add link to root endpoint for logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header: React.FC = () => {
   return (
     <div className="fixed top-0 z-30 h-16 w-full bg-slate-800 p-4">
-      <h1 className="text-2xl font-bold text-white">Movies Catalog</h1>
+      <Link href="/" className="text-2xl font-bold text-white">
+        Movies Catalog
+      </Link>
     </div>
   );
 };
