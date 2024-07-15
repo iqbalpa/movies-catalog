017748ae38049fcd6126abee595ffd36cd5655d2
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-15 13:05:13 +0700

    feat(watchlist): create delete movie from watchlist

--- src/api/watchlist.api.ts
+++ src/api/watchlist.api.ts
@@ -32,3 +32,18 @@ export const addToWatchlist = async (
   const movie: Movie = res.data;
   return movie;
 };
+
+export const deleteFromWatchlist = async (
+  accessToken: string,
+  movieId: number,
+): Promise<Movie> => {
+  const res = await axios.delete(`${BASE_URL}/movies/watchlist/${movieId}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const movie: Movie = res.data;
+  console.log('DELETED');
+  console.log(movie);
+  return movie;
+};
--- src/modules/watchlist/watchlist.tsx
+++ src/modules/watchlist/watchlist.tsx
@@ -2,13 +2,26 @@
 
 import React, { useEffect, useState } from 'react';
 import { getCookie } from 'cookies-next';
-import { getWatchlist } from '@/api/watchlist.api';
-import { SquareArrowOutUpRight } from 'lucide-react';
+import { deleteFromWatchlist, getWatchlist } from '@/api/watchlist.api';
+import { SquareArrowOutUpRight, Trash2 } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
 import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 
 interface Movie {
   id: number;
@@ -31,7 +44,6 @@ const WatchlistModule = () => {
       router.push('/signin');
       return;
     }
-
     const fetchWatchlist = async () => {
       try {
         const res = await getWatchlist(accessToken);
@@ -42,6 +54,15 @@ const WatchlistModule = () => {
     };
     fetchWatchlist();
   }, [user, accessToken, router]);
+
+  const handleDelete = async (movieId: number) => {
+    try {
+      await deleteFromWatchlist(accessToken, movieId);
+      toast.success('movie removed from watchlist');
+    } catch (e) {
+      console.log('failed to remove the movie from watchlist');
+    }
+  };
 
   return (
     <div className="flex min-h-screen w-full justify-center bg-black pb-14 pt-10 text-white md:pt-20">
@@ -76,13 +97,25 @@ const WatchlistModule = () => {
                     {movie.overview}
                   </p>
                 </div>
-                <Link
-                  href={`/${movie.id}`}
-                  target="_blank"
-                  className="rounded-md p-1 duration-150 hover:cursor-pointer hover:bg-slate-200 hover:bg-opacity-15"
-                >
-                  <SquareArrowOutUpRight />
-                </Link>
+                <div className="flex flex-row items-center justify-center gap-2 md:flex-col">
+                  <Link
+                    href={`/${movie.id}`}
+                    target="_blank"
+                    className="rounded-md p-1 duration-150 hover:cursor-pointer hover:bg-slate-200 hover:bg-opacity-15"
+                  >
+                    <SquareArrowOutUpRight />
+                  </Link>
+                  {/* <button
+                    onClick={() => handleDelete(movie.id)}
+                    className="rounded-md p-1 duration-150 hover:cursor-pointer hover:bg-red-500 hover:bg-opacity-40 hover:text-red-500"
+                  >
+                    <Trash2 />
+                  </button> */}
+                  <MyAlertDialog
+                    movieId={movie.id}
+                    handleDelete={handleDelete}
+                  />
+                </div>
               </div>
             ))}
           </div>
@@ -93,3 +126,36 @@ const WatchlistModule = () => {
 };
 
 export default WatchlistModule;
+
+interface IMyAlertDialog {
+  movieId: number;
+  handleDelete: (id: number) => void;
+}
+
+const MyAlertDialog: React.FC<IMyAlertDialog> = ({ movieId, handleDelete }) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger className="rounded-md p-1 duration-150 hover:cursor-pointer hover:bg-red-500 hover:bg-opacity-40 hover:text-red-500">
+        <Trash2 />
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => handleDelete(movieId)}
+            className="bg-red-500 hover:bg-red-500 hover:bg-opacity-85"
+          >
+            Remove
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
